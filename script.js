ed99399c53794ed9dacfaf71fbbb099cbe38358a
Quinn Kanner <quinn.kanner1@gmail.com>
2022-04-20 09:48:12 -0400

    Added files

---
+++ script.js
@@ -0,0 +1,132 @@
+//constants
+const clueHoldTime = 1000; //how long to hold each clue's light/sound
+const cluePauseTime = 333; //how long to pause in between clues
+const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+//Global Variables
+
+var pattern = [1,2,4,5,3,6,7,1,3];
+var progress = 0;
+var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.4; //must be between 0.0 and 1.0
+var guessCounter = 0;
+
+//functions
+
+function startGame() {
+  //initialize game variables
+  progress = 0;
+  gamePlaying = true;
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
+  playClueSequence();
+}
+function loseGame() {
+  endGame();
+  alert("Game Over. You lost.");
+}
+function winGame() {
+  endGame();
+  alert("Game Over. You win admission into the Futureforce Tech Launchpad :)");
+}
+function endGame() {
+  //initialize game variables
+  gamePlaying = false;
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+}
+const freqMap = {
+  1: 261.626,
+  2: 277.18,
+  3: 293.665,
+  4: 311.127,
+  5: 329.628,
+  6: 349.228,
+  7: 369.994,
+};
+function playTone(btn, len) {
+  o.frequency.value = freqMap[btn];
+  g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
+  context.resume();
+  tonePlaying = true;
+  setTimeout(function () {
+    stopTone();
+  }, len);
+}
+function startTone(btn) {
+  if (!tonePlaying) {
+    context.resume();
+    o.frequency.value = freqMap[btn];
+    g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
+    context.resume();
+    tonePlaying = true;
+  }
+}
+function stopTone() {
+  g.gain.setTargetAtTime(0, context.currentTime + 0.05, 0.025);
+  tonePlaying = false;
+}
+
+// Page Initialization
+// Init Sound Synthesizer
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+var o = context.createOscillator();
+var g = context.createGain();
+g.connect(context.destination);
+g.gain.setValueAtTime(0, context.currentTime);
+o.connect(g);
+o.start(0);
+
+function lightButton(btn) {
+  document.getElementById("btn" + btn).classList.add("lit");
+}
+function clearButton(btn) {
+  document.getElementById("btn" + btn).classList.remove("lit");
+}
+function playSingleClue(btn) {
+  if (gamePlaying) {
+    lightButton(btn);
+    playTone(btn, clueHoldTime);
+    setTimeout(clearButton, clueHoldTime, btn);
+  }
+}
+function playClueSequence() {
+  context.resume();
+  guessCounter = 0;
+  let delay = nextClueWaitTime; //set delay to initial wait time
+  for (let i = 0; i <= progress; i++) {
+    // for each clue that is revealed so far
+    console.log("play single clue: " + pattern[i] + " in " + delay + "ms");
+    setTimeout(playSingleClue, delay, pattern[i]); // set a timeout to play that clue
+    delay += clueHoldTime;
+    delay += cluePauseTime;
+  }
+}
+function guess(btn) {
+  console.log("user guessed: " + btn);
+  if (!gamePlaying) {
+    return;
+  }
+  if (pattern[guessCounter] == btn) {
+    //Guess was correct!
+    if (guessCounter == progress) {
+      if (progress == pattern.length - 1) {
+        //GAME OVER: WIN!
+        winGame();
+      } else {
+        //Pattern correct. Add next segment
+        progress++;
+        playClueSequence();
+      }
+    } else {
+      //so far so good... check the next guess
+      guessCounter++;
+    }
+  } else {
+    //Guess was incorrect
+    //GAME OVER: LOSE!
+    loseGame();
+  }
+
+}
